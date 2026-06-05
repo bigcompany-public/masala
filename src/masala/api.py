870d735c31fbb009cfe9ceed485d5ce159594114
Tristan Languebien <tlanguebien@gmail.com>
2026-06-05 14:55:50 +0200

    add the concept of entry point

--- src/masala/api.py
+++ src/masala/api.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Callable
+from typing import Any, Callable
 
 from lucent import Convention
 
@@ -24,15 +24,18 @@ class AssetBlock:
     def __init__(
         self,
         name: str,
+        label: str,
         description: str,
         convention: Convention,
         current_path_callback: Callable[..., Path],
         export_callback: Callable[..., Path],
-        load_callback: Callable[[Path | None], list[EntryPoint]],
+        load_callback: Callable[[AssetBlock, Path], None],
         assembly_callback: Callable,
         destination_path_callback: Callable[[AssetBlock], Path] | None = None,
+        entry_points: list[EntryPoint] | None = None,
     ) -> None:
         self.name = name
+        self.label = label or name.replace("_", " ").replace("-", " ").title()
         self.description = description
         self.convention = convention
         self.codex = convention._codex
@@ -41,9 +44,12 @@ class AssetBlock:
         self.export_callback = export_callback
         self.load_callback = load_callback
         self.assembly_callback = assembly_callback
+        entry_points = entry_points or []
+        self.loaded = False
+        self.entry_points: dict[str, EntryPoint] = {ep.name: ep for ep in entry_points}
 
-    def get_destination_path(self) -> Path:
-        return self.destination_path_callback(self)
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.label})"
 
     def get_current_path(self) -> Path:
         """Returns the path to the current scene"""
@@ -51,6 +57,9 @@ class AssetBlock:
 
     def get_current_fields(self) -> dict[str, str]:
         return self.codex.get_fields(self.get_current_path())  # type: ignore
+
+    def get_destination_path(self) -> Path:
+        return self.destination_path_callback(self)
 
     def export(self) -> Path:
         return self.export_callback(self)
@@ -63,12 +72,19 @@ class AssetBlock:
             fields.pop("description")
         return self.convention.get_last_path(fields)
 
-    def load(self, path: Path | None = None) -> list[EntryPoint]:
+    def load(self, path: Path | None = None):
         if not path:
             path = self.get_last_path()
-        return self.load_callback(path)
+        self.load_callback(self, path)
+        self.loaded = True
 
 
 class EntryPoint:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name: str, label: str = "", str="", description: str = "") -> None:
+        self.name = name
+        self.label = label or name.replace("_", " ").replace("-", " ").title()
+        self.description = description
+        self.object: Any = None
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.label}) -> {self.object}"
--- src/masala/example/asset_blocks/staticmesh.py
+++ src/masala/example/asset_blocks/staticmesh.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from masala.api import AssetBlock
+from masala.api import AssetBlock, EntryPoint
 from masala.example.codex import codex
 
 DOCUMENT = {"_id": "6a21916d8459ebb4f5618e41", "type": "lab", "asset": "elderSprite", "_aquariumKey": 755557654}
@@ -22,17 +22,21 @@ def assemble(assetblock: AssetBlock):
     print(f"Assembling {DOCUMENT['asset']}")
 
 
-def load(path: Path):
+def load(assetblock: AssetBlock, path: Path):
     print(f"Loading {path}")
-    return []
+    assetblock.entry_points["main_group"].object = "MAIN_GROUP"
 
+
+main_group = EntryPoint(name="main_group")
 
 static_mesh = AssetBlock(
-    "staticMesh",
-    description="Static Mesh",
+    "static_mesh",
+    label="Static Mesh",
+    description="Geometries of the asset, without materials, deformers...",
     convention=codex.convs.assetblock_static_mesh,
     current_path_callback=current_scene_callback,
     export_callback=export,
     assembly_callback=assemble,
     load_callback=load,
+    entry_points=[main_group],
 )
