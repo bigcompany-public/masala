2efa8ca0d1eacb18ec72e26cb26db140e2209230
Tristan Languebien <tlanguebien@gmail.com>
2026-06-10 12:30:27 +0200

    remove callbacks from the assetBlock class and create an assetBlockRegistry class

--- src/masala/api.py
+++ src/masala/api.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Any, Callable
+from typing import Callable
 
 from lucent import Convention
+
+
+class DuplicateAssetBlockError(Exception): ...
+
+
+class AssetBlockNotFoundError(Exception): ...
 
 
 def default_destination_path_callback(assetblock: AssetBlock) -> Path:
@@ -22,32 +28,24 @@ class AssetBlock:
         label: str,
         description: str,
         convention: Convention,
-        current_path_callback: Callable[..., Path],
-        export_callback: Callable[..., Path],
-        load_callback: Callable[[AssetBlock, Path], None],
-        assembly_callback: Callable,
         destination_path_callback: Callable[[AssetBlock], Path] | None = None,
-        entry_points: list[EntryPoint] | None = None,
     ) -> None:
         self.name = name
         self.label = label or name.replace("_", " ").replace("-", " ").title()
         self.description = description
         self.convention = convention
         self.codex = convention._codex
-        self.current_path_callback = current_path_callback
+        self.current_path_callback: Callable[..., Path] | None = None
         self.destination_path_callback = destination_path_callback or default_destination_path_callback
-        self.export_callback = export_callback
-        self.load_callback = load_callback
-        self.assembly_callback = assembly_callback
-        entry_points = entry_points or []
         self.loaded = False
-        self.entry_points: dict[str, EntryPoint] = {ep.name: ep for ep in entry_points}
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.label})"
+        return f"{self.__class__.__name__}({self.name})"
 
     def get_current_path(self) -> Path:
         """Returns the path to the current scene"""
+        if not self.current_path_callback:
+            raise AttributeError("Please provide a current_path_callback")
         return self.current_path_callback()
 
     def get_current_fields(self) -> dict[str, str]:
@@ -55,9 +53,6 @@ class AssetBlock:
 
     def get_destination_path(self) -> Path:
         return self.destination_path_callback(self)
-
-    def export(self) -> Path:
-        return self.export_callback(self)
 
     def get_last_path(self) -> Path:
         fields = self.get_current_fields()
@@ -67,24 +62,53 @@ class AssetBlock:
             fields.pop("description")
         return self.convention.get_last_path(fields)
 
-    def load(self, path: Path | None = None):
-        if not path:
-            path = self.get_last_path()
-        self.load_callback(self, path)
-        self.loaded = True
 
+class AssetBlockRegistry:
+    def __init__(self, assetblocks: list[AssetBlock]) -> None:
+        self._assetblocks: list[AssetBlock] = []
+        for assetblock in sorted(assetblocks, key=lambda x: x.name):
+            self.register_assetblock(assetblock)
+        self._iterindex: int = 0
 
-class EntryPoint:
-    def __init__(self, name: str, label: str = "", str="", description: str = "") -> None:
-        self.name = name
-        self.label = label or name.replace("_", " ").replace("-", " ").title()
-        self.description = description
-        self.object: Any = None
+    def register_assetblock(self, assetblock: AssetBlock):
+        if assetblock.name in self.get_assetblock_names():
+            raise DuplicateAssetBlockError(f'Multiple AssetBlocks with name "{assetblock.name}" cannot be registered')
+        if assetblock.label in self.get_assetblock_labels():
+            raise DuplicateAssetBlockError(f'Multiple AssetBlocks with label "{assetblock.label}" cannot be registered')
+        self._assetblocks.append(assetblock)
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.label}) -> {self.object}"
+        return (
+            f"{self.__class__.__name__}({len(self._assetblocks)} AssetBlock{'s' if len(self._assetblocks) > 1 else ''})"
+        )
 
+    def __iter__(self):
+        self._iterindex = len(self._assetblocks)
+        return self
 
-class Assembler:
-    def __init__(self) -> None:
-        pass
+    def __next__(self):
+        if self._iterindex == 0:
+            raise StopIteration
+        self._iterindex = self._iterindex - 1
+        return self._assetblocks[self._iterindex]
+
+    def get_assetblock_names(self) -> list[str]:
+        return [assetblock.name for assetblock in self._assetblocks]
+
+    def get_assetblock_labels(self) -> list[str]:
+        return [assetblock.label for assetblock in self._assetblocks]
+
+    def get_assetblock_by_name(self, name: str) -> AssetBlock:
+        assetblocks = [assetblock for assetblock in self._assetblocks if assetblock.name == name]
+        if not assetblocks:
+            raise AssetBlockNotFoundError(f'AssetBlock name not found : "{name}"')
+        return assetblocks[0]
+
+    def get_assetblock_by_label(self, label: str) -> AssetBlock:
+        assetblocks = [assetblock for assetblock in self._assetblocks if assetblock.label == label]
+        if not assetblocks:
+            raise AssetBlockNotFoundError(f'AssetBlock label not found : "{label}"')
+        return assetblocks[0]
+
+    def __getitem__(self, key: str):
+        return self.get_assetblock_by_name(key)
--- src/masala/example/asset_blocks/staticmesh.py
+++ src/masala/example/asset_blocks/staticmesh.py
@@ -1,42 +1,19 @@
 from pathlib import Path
 
-from masala.api import AssetBlock, EntryPoint
+from masala.api import AssetBlock
 from masala.example.codex import codex
 
 DOCUMENT = {"_id": "6a21916d8459ebb4f5618e41", "type": "lab", "asset": "elderSprite", "_aquariumKey": 755557654}
 
 
-def current_scene_callback():
-    return codex.convs.asset_modeling_workfile.get_last_path(DOCUMENT)
+def custom_path(assetblock: AssetBlock) -> Path:
+    return Path("D:/remove_me/staticmesh.usd")
 
-
-def export(assetblock: AssetBlock):
-    path = assetblock.get_destination_path()
-    print(f"Exporting {DOCUMENT['asset']} to {path}")
-    path.parent.mkdir(exist_ok=True, parents=True)
-    path.write_text("Hello World")
-    return path
-
-
-def assemble(assetblock: AssetBlock):
-    print(f"Assembling {DOCUMENT['asset']}")
-
-
-def load(assetblock: AssetBlock, path: Path):
-    print(f"Loading {path}")
-    assetblock.entry_points["main_group"].object = "MAIN_GROUP"
-
-
-main_group = EntryPoint(name="main_group")
 
 static_mesh = AssetBlock(
-    "static_mesh",
+    name="static_mesh",
     label="Static Mesh",
     description="Geometries of the asset, without materials, deformers...",
     convention=codex.convs.assetblock_static_mesh,
-    current_path_callback=current_scene_callback,
-    export_callback=export,
-    assembly_callback=assemble,
-    load_callback=load,
-    entry_points=[main_group],
+    destination_path_callback=custom_path,
 )
