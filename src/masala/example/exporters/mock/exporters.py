58bc05487f6e3531f0d5a91d51cb4e6a22d0d1e1
Tristan Languebien <tlanguebien@gmail.com>
2026-06-29 11:25:40 +0200

    add mock exporters

---
+++ src/masala/example/exporters/mock/exporters.py
@@ -0,0 +1,3 @@
+from masala.example.exporters.mock.static_mesh_exporter import static_mesh_exporter
+
+exporters = [static_mesh_exporter]
---
+++ src/masala/example/exporters/mock/static_mesh_exporter.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from masala.api import Exporter
+from masala.example.asset_blocks.staticmesh import static_mesh
+
+
+def get_path() -> Path:
+    return Path("//srv-bc-fs1/Norman/assetWorkspace/lab/elderSprite/mdl/blender/elderSprite_mdl_v001_init.blend")
+
+
+def export(path: Path):
+    print(f"Writing placeholder file to {path}")
+    path.parent.mkdir(exist_ok=True, parents=True)
+    path.write_text("placeholder")
+
+
+def meta() -> dict:
+    return {"hello": "world"}
+
+
+static_mesh_exporter = Exporter(
+    static_mesh, current_path_callback=get_path, export_callback=export, metadata_callback=meta
+)
