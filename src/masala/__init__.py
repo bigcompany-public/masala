df274ec3117824aee0be5f7f5826cff9cef3491d
Tristan Languebien <tlanguebien@gmail.com>
2026-07-06 13:05:00 +0200

    simplify imports

--- src/masala/__init__.py
+++ src/masala/__init__.py
@@ -1,2 +1,9 @@
-def main() -> None:
-    print("Hello from masala!")
+from masala.api import AssetBlock, Exporter, Input, Operator, Output
+
+__all__ = [
+    "AssetBlock",
+    "Exporter",
+    "Input",
+    "Output",
+    "Operator",
+]
